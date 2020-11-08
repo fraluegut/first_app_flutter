f1b6686fae83496cff822aa648e790430ea659a8
fraluegut <franciscojavierluengo@outlook.com>
2020-11-09 00:59:58 +0100

    AnimatedContainer

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(2.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://avatars3.githubusercontent.com/u/36550503?s=400&u=82ca522a3db817fc760c7300275d4106fb7849c7&v=4"),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("SL"),
+              backgroundColor: Colors.brown,
+              
+            ),
+          )
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://avatars3.githubusercontent.com/u/36550503?s=400&u=82ca522a3db817fc760c7300275d4106fb7849c7&v=4"),
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          fadeInDuration: Duration( milliseconds: 200),
+        ),
+      )
     );
   }
 }
