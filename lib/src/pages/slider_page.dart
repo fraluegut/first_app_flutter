9b9f78a24e58bd0add67923b9c3a4ec3317bff5e
fraluegut <franciscojavierluengo@outlook.com>
2020-11-09 16:41:25 +0100

    Final commit

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -6,17 +6,87 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Slider"),
-
       ),
       body: Container(
-        child: Text("Slider Page"),
-      ),
-      
+          padding: EdgeInsets.only(top: 50.0),
+          child: Column(
+            children: <Widget>[
+              _crearSlider(),
+              _checkBox(),
+              _crearSwitch(),
+              Expanded(child: _crearImagen()),
+            ],
+          )),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: "Tamaño de la imagen",
+      //divisions: 20,
+      value: _valorSlider,
+      min: 10.0,
+      max: 400.0,
+      onChanged: ( _bloquearCheck) ? null:
+       (valor) {
+        setState(() {
+          _valorSlider = valor;
+        });
+      },
+    );
+  }
+  /*
+  Widget _checkBox() {
+    return Checkbox(
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }*/
+
+  Widget _checkBox() {
+    return CheckboxListTile(
+      title: Text("Bloquear slider"),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearSwitch(){
+    return SwitchListTile(
+      title: Text("Bloquear slider"),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+    
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage(
+          "https://avatars3.githubusercontent.com/u/36550503?s=400&u=82ca522a3db817fc760c7300275d4106fb7849c7&v=4"),
+      width: _valorSlider,
+      fit: BoxFit.contain,
     );
   }
 }
